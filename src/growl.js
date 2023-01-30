13983eb028452a506f3e501e0f8e354581079612
JSonHP <jsonhp@JSonHPs-Mac.local>
2023-01-29 16:18:32 -0800

    hook modified

---
+++ src/growl.js
@@ -0,0 +1,27 @@
+import React from 'react'
+
+import './growl.css'
+
+export const Growl = ({ active, message, onDismissed }) => (
+  <div className={`growl${active ? " active" : ""}`}>
+    {message}
+    <div onClick={onDismissed} className="growl-close" />
+  </div>
+)
+
+
+export function useGrowl(timeout = 3000) {
+  // state of the growl
+  const [growlActive, setGrowlActive] = React.useState(false)
+
+  return [
+    // the first arg is the state
+    growlActive,
+
+    // the second arg is a fn that allows you to safely set its state
+    (active) => {
+      setGrowlActive(active)
+      setTimeout(() => { setGrowlActive(false) }, timeout)
+    },
+  ]
+}
